b71434fe96affe81e1f3894aeb27e88f0f64e4e4
Pavel <virus3740@gmail.com>
2022-05-20 11:50:10 +0300

    4 модуль 1 задание

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,13 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.body;
+  let ul = document.createElement('ul');
+  document.body.appendChild(ul);
+  friends = friends.map(elem => elem.firstName + ' ' + elem.lastName);
+  for(let i = 0; i<friends.length; i++){
+      let li = document.createElement('li');
+      li.textContent = friends[i];
+      ul.appendChild(li);
+      console.log(friends.length);
+    }
+    return ul;
 }
